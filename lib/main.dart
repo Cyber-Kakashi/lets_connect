39bae8c2d9f3608690fe38ff9d4765d3e151d483
komalswami <komalsswami@gmail.com>
2021-06-20 22:52:59 +0530

    initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lets_connect/src/chatScreen.dart';
+import 'package:lets_connect/src/myHomePage.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,15 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Lets Connect',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Lets connect'),
-        ),
-        body: const Center(
-          child: Text('Lets connect'),
-        ),
-      ),
+        home: Scaffold(
+    body: MyHomePage(),)
     );
   }
 }
